6499b5099a98e541305cd7d3bdae8be68421ad13
Jessica Garza <jdgarza@cs.uchicago.edu>
2019-04-17 14:09:05 -0500

    Added app icon and readme

--- ItsAZooInThere/ViewController.swift
+++ ItsAZooInThere/ViewController.swift
@@ -13,16 +13,18 @@ import AVFoundation
 class ViewController: UIViewController {
     @IBOutlet weak var speciesLabel: UILabel!
     @IBOutlet weak var scrollView: UIScrollView!
+    
+    // MARK: Properties
     var animals = [Animal]()
     var audioPlayer: AVAudioPlayer?
     
+    // MARK: Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         scrollView.contentSize = CGSize(width: 1125, height: 500)
-        
         scrollView.delegate = self
-
+        
+        // MARK: Animal assets
         let pigImage = UIImage(named: "Pig")!
         let pigSoundPath = Bundle.main.path(forResource: "ofarm.mp3", ofType:nil)
         let cowImage = UIImage(named: "Cow")!
@@ -30,14 +32,13 @@ class ViewController: UIViewController {
         let wolfImage = UIImage(named: "Wolf")!
         let wolfSoundPath = Bundle.main.path(forResource: "wolf5.mp3", ofType:nil)
         
+        // MARK: Animal bjects
         let pig = Animal(name: "Gary", species: "Pig", age: 7, image: pigImage, soundPath: pigSoundPath)
         let cow = Animal(name: "Larry", species: "Cow", age: 21, image: cowImage, soundPath: cowSoundPath)
         let wolf = Animal(name: "Jerry", species: "Wolf", age: 7, image: wolfImage, soundPath: wolfSoundPath)
         
         animals += [pig, cow, wolf]
-        
         animals.shuffle()
-
         
         speciesLabel.text = animals[0].species
         
@@ -45,7 +46,6 @@ class ViewController: UIViewController {
             let button = UIButton(type: .system)
             let buttonRect = CGRect(origin:  CGPoint(x: 140 + (i * 375), y: 400), size: CGSize(width: 100, height: 100))
             button.frame = buttonRect
-//            button.backgroundColor = UIColor.blue
             scrollView.addSubview(button)
             let animal = animals[i]
             let image = UIImageView(image: animal.image)
@@ -57,17 +57,14 @@ class ViewController: UIViewController {
             scrollView.addSubview(image)
         }
     }
-    
+    // MARK: Button Actions
     @objc func buttonTapped(sender: UIButton) {
-        print("HEY \(sender.tag) IS CLICKING")
-        
         let animal = animals[sender.tag]
         print("Animal: name = \(animal.name), species = \(animal.species), age = \(animal.age)")
         let alertController = UIAlertController(title: " ", message:
             "Animal: name = \(animal.name), species = \(animal.species), age = \(animal.age)", preferredStyle: .alert)
         alertController.addAction(UIAlertAction(title: "Done", style: .default))
         self.present(alertController, animated: true, completion: nil)
-        
         do {
             audioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: animal.soundPath))
             audioPlayer?.play()
@@ -81,15 +78,19 @@ class ViewController: UIViewController {
 extension ViewController: UIScrollViewDelegate {
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
         var animal = animals[0]
+        
         var offset = Int(scrollView.contentOffset.x)
         var screen = Int(offset / 375)
+        
         animal = animals[ screen ]
+        
         offset = offset % 375
+        
         let opacity = CGFloat(offset) / 188.0
-        print("offset: \(offset)")
+        
         speciesLabel.text = animal.species
         speciesLabel.alpha = 1 - opacity
-        print("opacity: \(opacity)")
-    print("(\(scrollView.contentOffset.x),\(scrollView.contentOffset.y))")
+        
+        print("(\(scrollView.contentOffset.x),\(scrollView.contentOffset.y))")
     }
 }
---
+++ README.md
@@ -0,0 +1,5 @@
+Sources:
+
+https://www.hackingwithswift.com/example-code/media/how-to-play-sounds-using-avaudioplayer
+https://developer.apple.com/documentation/uikit/uiscrollview
+https://developer.apple.com/documentation/uikit/uibutton
